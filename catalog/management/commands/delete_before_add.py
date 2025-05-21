0a5a2733c4075ffdc4cce5a5e7d02a8431e722bb
Даниил Ермилов <yndx-daniel-ocean@yandex.ru>
2025-05-21 17:45:26 +0300

    add models

---
+++ catalog/management/commands/delete_before_add.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from catalog.models import Product, Category
+
+class Command(BaseCommand):
+    help = 'Delete data'
+
+    def handle(self, *args, **options):
+        # Удаляем существующие записи
+        Product.objects.all().delete()
+        Category.objects.all().delete()
--- config/settings.py
+++ config/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'catalog',
+        'USER': 'postgres',
+        'PASSWORD': '3228',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

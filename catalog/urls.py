40c773bcea251d49ab47cf9e1286e55d775a6a76
Даниил Ермилов <yndx-daniel-ocean@yandex.ru>
2025-05-23 23:20:44 +0300

    24 HW init

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,5 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("home/", views.show_home, name="home"),
     path("contacts/", views.show_contacts, name="contacts"),
+    path("product_detail/", views.product_detail, name="product_detail")
 ]

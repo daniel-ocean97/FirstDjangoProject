997d970994ee20dc4070c6a4d808902c4be07402
Даниил Ермилов <yndx-daniel-ocean@yandex.ru>
2025-07-05 23:53:22 +0300

    25 HW done

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from blog.apps import BlogConfig
+
+app_name = BlogConfig.name
+
+urlpatterns = [
+    path("blog/", views.BlogListView.as_view(), name="blog"),
+    path("blog/create", views.BlogCreateView.as_view(), name="post_create"),
+    path("blog/detail/<int:pk>/", views.BlogDetailView.as_view(), name="post_detail"),
+    path("blog/update/<int:pk>/", views.BlogUpdateView.as_view(), name="post_update"),
+    path("blog/delete/<int:pk>/", views.BlogDeleteView.as_view(), name="post_delete")
+]
